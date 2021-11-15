cb5aa99bd88c19cdb27a2b03d6bb9c332a7dcc2f
mgvocana <44788832+mgvocana@users.noreply.github.com>
2021-11-15 11:38:44 -0700

    Finished making it do a whole lot of nothing

--- DropGame/DropGameView.swift
+++ DropGame/DropGameView.swift
@@ -6,16 +6,33 @@
 //
 
 import SwiftUI
+import SpriteKit
 
-struct DropGameView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct DropGameView: View
+{
+    let height : CGFloat = 300
+    let width : CGFloat = 400
+    
+    private var simpleScene : GameScene
+    {
+        let scene = GameScene()
+        scene.size = CGSize(width: width, height: height)
+        scene.scaleMode = .fill
+        return scene
+    }
+    
+    var body: some View
+    {
+        SpriteView(scene: simpleScene)
+            .frame(width: width, height: height)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
         DropGameView()
     }
 }
