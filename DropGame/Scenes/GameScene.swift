db4a8a35f99e6ab5708701b52ca4d5f77cb95715
mgvocana <44788832+mgvocana@users.noreply.github.com>
2021-11-15 12:00:13 -0700

    Gave the blocks more colors

--- DropGame/Scenes/GameScene.swift
+++ DropGame/Scenes/GameScene.swift
@@ -18,7 +18,7 @@ class GameScene : SKScene
     {
         guard let touch = touches.first
         else {return}
-        let currentColor = UIColor.purple
+        let currentColor = assignColorAndBitmask()
         let width = Int(arc4random() % 50)
         let height = Int(arc4random() % 50)
         let location = touch.location(in: self)
@@ -29,5 +29,13 @@ class GameScene : SKScene
         node.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: width, height: height))
         
         addChild(node)
+    }
+    
+    private func assignColorAndBitmask() -> UIColor
+    {
+        let colors : [UIColor] = [.cyan, .purple, .green, .magenta, .blue, .yellow]
+        let randomIndex = Int(arc4random()) % colors.count
+        
+        return colors[randomIndex]
     }
 }
