d036401ac38efae9927ad7e5287a738c7f3d6751
mgvocana <44788832+mgvocana@users.noreply.github.com>
2021-11-15 11:26:26 -0700

    Created new project

---
+++ DropGame/DropGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropGameApp.swift
+//  DropGame
+//
+//  Created by Vocana, Maya on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct DropGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DropGameView()
+        }
+    }
+}
---
+++ DropGame/DropGameView.swift
@@ -0,0 +1,21 @@
+//
+//  DropGameView.swift
+//  DropGame
+//
+//  Created by Vocana, Maya on 11/15/21.
+//
+
+import SwiftUI
+
+struct DropGameView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        DropGameView()
+    }
+}
